1b1433ea4afd081d82e5391d6785135d041941fa
Herbert Yang <herbyang@amazon.com>
2020-02-02 15:44:29 +0800

    Update pandas script to successfully produce 3 charts

--- scripts/plot_chart.py
+++ scripts/plot_chart.py
@@ -3,21 +3,42 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
-lifeToDate = pd.read_csv('../data/pandemic2020.csv')
+df = pd.read_csv(
+	'../data/pandemic2020.csv',
+	dtype={
+		'Close Contact': 'float',
+		'In Treatment': 'float',
+		'Suspected Infection': 'float',
+		'Confirmed Infection': 'float',
+		'Severe Infection': 'float',
+		'Death': 'float',
+		'Cured': 'float'
+		}
+	)
+df['Date'] = pd.to_datetime(df['Date'])
 
-netNew = lifeToDate.set_index('Date').diff()
-# print(netNew)
+# print(df)
+# print(df.dtypes)
+
+df['Close Contact New'] = df['Close Contact'].diff()
+df['In Treatment New'] = df['In Treatment'].diff()
+df['Suspected Infection New'] = df['Suspected Infection'].diff()
+df['Confirmed Infection New'] = df['Confirmed Infection'].diff()
+df['Severe Infection New'] = df['Severe Infection'].diff()
+df['Death New'] = df['Death'].diff()
+df['Cured New'] = df['Cured'].diff()
+
+# print(df)
+
+# print(df.dtypes)
 
 plt.rcParams["font.family"]= "Heiti TC"
 
 # m: magenta, b: blue, c: cyan, r: red
 
+# ------------ Plot Life-To-Date Chart for the Big 4 --------------------------
 
-# set daily locator
-# chart.xaxis.set_major_locator(mdates.DayLocator(interval=1))
-# chart.xaxis.set_major_formatter(mdates.DateFormatter('%d-%m'))
-
-chartLTD = lifeToDate.plot(
+fig = df.plot(
 	x='Date', 
 	y=['Suspected Infection', 'Confirmed Infection', 'Severe Infection','Death'],
 	style=['m', 'b', 'c', 'r'],
@@ -26,18 +47,44 @@ chartLTD = lifeToDate.plot(
 plt.title('2020年新型冠状病毒感染趋势图 - 累计数据')
 plt.xlabel('日期')
 plt.ylabel('累计人数')
+# fig.xaxis.set_major_locator(mdates.DayLocator(interval=1))
+# fig.xaxis.set_major_formatter(mdates.DateFormatter('%m/%d'))
 # plt.show()
 plt.savefig('../charts/chart_big_4_ltd.png')
 plt.close()
 
-# chartNW = netNew.plot(
-# 	x='Date', 
-# 	y=['Suspected Infection', 'Confirmed Infection', 'Severe Infection','Death'],
-# 	style=['m', 'b', 'c', 'r'],
-# 	linewidth=3
-# )
-# plt.title('2020年新型冠状病毒感染趋势图 - 每日新增数据')
-# plt.xlabel('日期')
-# plt.ylabel('新增人数')
+# ------------ Plot Daily Net New Chart for the Big 4 -------------------------
+
+df.plot(
+	x='Date', 
+	y=['Suspected Infection New', 'Confirmed Infection New', 'Severe Infection New','Death New'],
+	style=['m', 'b', 'c', 'r'],
+	linewidth=3
+)
+plt.title('2020年新型冠状病毒感染趋势图 - 每日新增')
+plt.xlabel('日期')
+plt.ylabel('新增人数')
+# fig.xaxis.set_major_locator(mdates.DayLocator(interval=2))
+# fig.xaxis.set_major_formatter(mdates.DateFormatter('%m-%d'))
 # plt.show()
-# plt.savefig('../charts/chart_big_4_net_new.png')
+plt.savefig('../charts/chart_big_4_net_new.png')
+plt.close()
+
+# ------------ Plot Life-to-Date for Death and Cured --------------------------
+
+df.plot(
+	x='Date', 
+	y=['Death', 'Cured'],
+	style=['r', 'g'],
+	linewidth=3
+)
+plt.title('2020年新型冠状病毒感染趋势图 - 死亡和治愈')
+plt.xlabel('日期')
+plt.ylabel('累计人数')
+# fig.xaxis.set_major_locator(mdates.DayLocator(interval=3))
+# fig.xaxis.set_major_formatter(mdates.DateFormatter('%m%d'))
+# plt.show()
+plt.savefig('../charts/chart_DnC_LTD.png')
+plt.close()
+
+
