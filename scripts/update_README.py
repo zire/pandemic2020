5c3110887ee4075822ef04fc4b9a2416b28b5baa
Herbert Yang <herbyang@amazon.com>
2020-02-04 10:35:19 +0800

    fix display of tables with thousand separator and percentages

--- scripts/update_README.py
+++ scripts/update_README.py
@@ -117,12 +117,21 @@ df_ltd = df[['日期', '接触', '观察', '疑似', '确诊', '重症', '死亡
 
 # print(df_ltd['日期'].strftime('%m/%d'))
 
+df_ltd['接触'] = df_ltd['接触'].map('{:,.0f}'.format)
+df_ltd['观察'] = df_ltd['观察'].map('{:,.0f}'.format)
+df_ltd['疑似'] = df_ltd['疑似'].map('{:,.0f}'.format)
+df_ltd['确诊'] = df_ltd['确诊'].map('{:,.0f}'.format)
+df_ltd['重症'] = df_ltd['重症'].map('{:,.0f}'.format)
+df_ltd['死亡'] = df_ltd['死亡'].map('{:,.0f}'.format)
+df_ltd['治愈'] = df_ltd['治愈'].map('{:,.0f}'.format)
+
 ltd_table = tabulate(
 	df_ltd,
 	headers = ['日期', '接触', '观察', '疑似', '确诊', '重症', '死亡', '治愈'],
 	showindex = 'always',
 	tablefmt = 'pipe',
-	colalign = 'right'
+	colalign = 'right',
+	numalign = 'right'
 )
 
 # print(ltd_table)
@@ -130,6 +139,15 @@ ltd_table = tabulate(
 df_change = df[[
 	'日期', '接触_新增率', '观察_新增率', '疑似_新增率', '确诊_新增率', '重症_新增率', '死亡_新增率', '治愈_新增率'
 	]].iloc[::-1]
+
+df_change['接触_新增率'] = df_change['接触_新增率'].map('{:,.2%}'.format)
+df_change['观察_新增率'] = df_change['观察_新增率'].map('{:,.2%}'.format)
+df_change['疑似_新增率'] = df_change['疑似_新增率'].map('{:,.2%}'.format)
+df_change['确诊_新增率'] = df_change['确诊_新增率'].map('{:,.2%}'.format)
+df_change['重症_新增率'] = df_change['重症_新增率'].map('{:,.2%}'.format)
+df_change['死亡_新增率'] = df_change['死亡_新增率'].map('{:,.2%}'.format)
+df_change['治愈_新增率'] = df_change['治愈_新增率'].map('{:,.2%}'.format)
+
 
 change_table = tabulate(
 	df_change,
